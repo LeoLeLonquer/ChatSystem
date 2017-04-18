6504984f4dbe4b61956b15ad890feda86c45d897
OulianaBadaoui <obadaoui@etud.insa-toulouse.fr>
2017-04-18 09:23:40 +0200

    Add files via upload

--- model/AllUsers.java
+++ model/AllUsers.java
@@ -1,8 +1,15 @@
-package model;
-
-import java.util.ArrayList;
+package model; 
+import java.util.*;
 
 public class AllUsers {
-	private ArrayList<User> ListUser ;
+	private ArrayList<User> ListUsers ;
 	
+	public AllUsers() { 
+		ListUsers = new ArrayList<User>() ; 
+	}
+
+	public boolean checkAvailable (User us) {
+		return us.getStatus(us); 
+	}
+
 }
