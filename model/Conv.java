fd09ee03502787d86cefceede7059ca51e946c9c
LeoLeLonquer <leolouis.le-lonquer@laposte.net>
2017-04-18 10:51:22 +0200

    Conv

--- model/Conv.java
+++ model/Conv.java
@@ -1,12 +1,63 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+
+import model.Message.DataType;
 
 public class Conv {
-	private Dest Dest;
+
 	ArrayList<Message> listeMessage;
 	
+	
 	public Conv(){
+		listeMessage=new ArrayList<Message>(100);
+	}
+
+	public void addMessage(Message msg){
+		listeMessage.add(msg);
+	}
+	
+	public void deleteConv(){
+		listeMessage.clear();
+	}
+	
+	public String readAllConv(){
+		String str="";
+		Message msg;
+		
+		Iterator <Message> itr = listeMessage.iterator();
+
+		while(itr.hasNext()){
+			msg= itr.next();
+			if (msg.getType()==DataType.Text){
+				str=str+"\n-"+msg.getSrcPseudo()+msg.getData();				
+			}
+			else if (msg.getType()==DataType.File){
+				//TODO
+			}
+			
+		}
+		return str;
+	}
+	
+	public String readLastMessage(){
+		String str="";
+		
+		int lastIndex= listeMessage.lastIndexOf(listeMessage);
+		Message msg= listeMessage.get(lastIndex);
+		
+		if (msg.getType()==DataType.Text){
+			str=str+"\n-"+msg.getSrcPseudo()+" : "+msg.getData();				
+		}
+		else if (msg.getType()==DataType.File){
+			//TODO
+		}
+		
+		return str;
+
+		
 		
 	}
+
 }
