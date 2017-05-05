35c621852995eec3fd7ac0c13dffef4e7eeac3cd
OulianaBadaoui <obadaoui@etud.insa-toulouse.fr>
2017-05-05 16:22:12 +0200

    Add files via upload

---
+++ controller/NotifyViewUsers.java
@@ -0,0 +1,22 @@
+package controller;
+import model.*; 
+
+import java.util.HashMap;
+
+import model.User;
+import view.ListUsers;
+
+public class NotifyViewUsers {
+	
+	private AllDests ad; 
+	private HashMap<Integer,User> theListus = ad.getListUsers(); 
+	
+	public void addConnectedUser (User newUs, ListUsers lu){ 
+		
+	}
+	
+	public void updateListUsers(ListUsers lu){ 
+		
+	}
+
+}
