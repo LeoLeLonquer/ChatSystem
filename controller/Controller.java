160df7a46c1a463381bfab949745fd87d4b18387
OulianaBadaoui <obadaoui@etud.insa-toulouse.fr>
2017-05-06 21:55:13 +0200

    Add files via upload

--- controller/Controller.java
+++ controller/Controller.java
@@ -9,107 +9,102 @@ import java.util.ArrayList;
 import javax.swing.JButton;
 
 import model.*;
+import view.Interface;
 import view.ListUsers;
 import view.LoginWindow;
 
 public class Controller {
 
-	SystemState sysState;
-	//Graphics interface;
-	private AllDests ad;
-	private ArrayList<User> ListUsersForView;
+	private SystemState sysState;
+	private Interface intf;
 
-
-	Controller(){
-
+	
+	public Controller(){
+		
 	}
-
-
+	
+	
 	public static void main(String[] args) {
 		Controller controller = new Controller();
-		 LoginWindow win = new LoginWindow(controller, "Hello! ");
+		Interface intf = new Interface(); 
+		 //LoginWindow win = new LoginWindow("Hello! ");
 		 }
-
+	
 	//***************interface model-Controller************************
-
+	
 	public void notifyNewMessageFromModel(int id) {
-
+		
 	}
 
-
-
+	
+	public boolean checkNewUser(ArrayList<User> ListofUsers){
+		
+		return false;
+	}
+	
 	//**************************************************************
-
+	
 	//***************interface view-Controller************************
-
-	public void sendMessage (String current, String destination, String text){
-
+	
+	public void sendMessage (String currentUser, String destUser, String msg){
+		
 	}
-
-	public void sendFile (String current, String destination, File file){
-
+	
+	public boolean askStatusOf(String user){
+		boolean status = false; 
+		return status; 
 	}
-
-	public void addMsgToLog (String text, File log){ // en m�me temps que le msg est load� dans la convo, il est ajout� dans le log
-
+	
+	public void sendFile (String current, String destination, File file){ 
+		
 	}
-
-	public User getCurrentUser(){
-		return this.sysState.getLoggedUser();
+	
+	public User getCurrentUser(){ 
+		return this.sysState.getLoggedUser(); 
 	}
-
-
-	 public void initUs (String pseudo){
-			SystemState sys = new SystemState(pseudo);
+	
+	 
+	 public void initUs (String pseudo){ 
+			SystemState sys = new SystemState(pseudo); 
 	 }
 
-	public void addConnectedUser (User newUs, ListUsers lu){
-
+	public void logOutFromLoggedUser(){
+		
 	}
-
-	public void updateListUsers(ListUsers lu){
-
+	
+	public void gotNewUser(User newUser){
+		this.intf.addNewUser(newUser.getPseudo());
 	}
-
-	public ArrayList<User> getListPseudos (){
-		return this.ListUsersForView;
-	}
-
-	public ArrayList<User> testListUsersView(){
-		Inet4Address IP1;
-		Inet4Address IP2;
-		Inet4Address IP3;
-		User u1;
-		User u2;
-		User u3;
-		ArrayList<User> al = new ArrayList<User>();
-
-		try {
-			IP1 = (Inet4Address) InetAddress.getLocalHost();
-			IP2 = (Inet4Address) InetAddress.getByName("192.168.11.2");
-			IP3 = (Inet4Address) InetAddress.getByName("192.168.11.3");
-
-
-			u1 = new User("Does", IP1, true);
-			u2 = new User("It", IP2, true);
-			u3 = new User("Work?", IP3, false);
-
-			al.add(u1);
-			al.add(u2);
-			al.add(u3);
-
-		} catch (UnknownHostException e) {
-			System.out.println("ERROR: Unkown Host ");
-		}
+	
+	public ArrayList <String> getListOfConnectedUsers (){
+		ArrayList<String> al = new ArrayList<String>();
 		return al;
 	}
-
-
-//	public void askConv(int id){
-//
-//	}
-
-
-
-
+	
+	/*************************************** TEST METHODS ************************************/
+	
+	// makes a list of connected users upon first connecting 
+	public ArrayList<String> testListUsersEmpty (){ 
+		ArrayList<String> al = new ArrayList<String>(); 
+		return al; 
+	}
+	
+	public ArrayList<String> testListUsersView(){
+		String u1; 
+		String u2; 
+		String u3; 
+		ArrayList<String> al = new ArrayList<String>(); 	
+		
+			u1 = "Does";
+			u2 ="It"; 
+			u3 = "Work?"; 
+			
+			al.add(u1); 
+			al.add(u2); 
+			al.add(u3); 
+		return al; 
+	}
+	
+	
+	
 }
---
+++ controller/TestAddUser.java
@@ -0,0 +1,30 @@
+package controller;
+
+import view.Interface;
+
+public class TestAddUser {
+	private Interface itf; 
+
+	public TestAddUser() {
+		itf = new Interface(); 
+	}
+	
+	public void aNewUserArrived(String newUser){
+		this.itf.addNewUser(newUser);
+	}
+	
+	public static void main(String args[]){
+		TestAddUser tau = new TestAddUser(); 
+		
+				try {
+					Thread.sleep(7000);
+					
+					tau.itf.addNewUser("Marie");
+					
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		
+	
+}
